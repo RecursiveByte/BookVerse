ab0c107a1cbae05e9024a4e90df0134e6cfcf8a2
RecursiveByte <opboy4885@gmail.com>
2026-02-25 01:04:12 +0530

    Update prisma.ts

--- backend/src/lib/prisma.ts
+++ backend/src/lib/prisma.ts
@@ -4,12 +4,7 @@ import { PrismaClient } from "../../generated/prisma/client"
 
 const connectionString = `${process.env.DATABASE_URL}`
 
-const adapter = new PrismaPg({ 
-  connectionString,
-  ssl: {
-     rejectUnauthorized: false
-  }
-})
+const adapter = new PrismaPg({ connectionString })
 
 const prisma = new PrismaClient({ adapter })
 export { prisma }
