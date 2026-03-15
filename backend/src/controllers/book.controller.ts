45f8bb9a042980530c77318c34a231a0f4bff45d
Retesh <opboy4885@gmail.com>
2026-03-15 19:02:47 +0530

    final push

--- backend/src/controllers/book.controller.ts
+++ backend/src/controllers/book.controller.ts
@@ -3,46 +3,6 @@ import HTTP_STATUS from "../utils/statusCodes";
 import { bookService } from "../services/book.service";
 import { Request, Response } from "express";
 
-// export const getBooksWithReviews = async (req: Request, res: Response) => {
-  // try {
-    // const page = Number(req.query.page) || 1;
-    // const limit = 24;
-// 
-    // const books = await bookService.getBooksWithReviews(page, limit);
-    // 
-    // return res.status(HTTP_STATUS.OK).json({ books });
-  // } catch (error: any) {
-    // logger.error("Error in getBooksWithReviews controller:", error);
-    // return res
-      // .status(HTTP_STATUS.INTERNAL_SERVER_ERROR)
-      // .json({ message: "Server error" });
-  // }
-// };
-// 
-// 
-// export const booksCount = async (req: Request, res: Response) => {
-  // try {
-  // const PAGE_SIZE = 8;
-  // 
-  // const totalBooks = await bookService.booksCount();
-  // 
-  // const lastPage = Math.ceil(totalBooks / PAGE_SIZE);
-  // 
-  // return res.status(HTTP_STATUS.OK).json({
-    // totalBooks,
-    // lastPage
-  // });
-  // 
-  // } catch (error: any) {
-  // logger.error("Error in booksCount controller:", error);
-  // 
-  // return res
-    // .status(HTTP_STATUS.INTERNAL_SERVER_ERROR)
-    // .json({ message: "Server error" });
-  // 
-  // }
-  // };
-
 
 export const getBooksWithReviews = async (req: Request, res: Response) => {
   try {
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -15,7 +15,7 @@ dotenv.config();
 const app = express();
 app.set("trust proxy", 1);
 
-// app.use("/api/auth", authRateLimiter);
+app.use("/api/auth", authRateLimiter);
 app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
 app.use(cookieParser());
 app.use(express.json());
@@ -32,11 +32,8 @@ app.listen(PORT, async () => {
   console.log(`Server running on http://localhost:${PORT}`);
 
   try {
-    // const startTime = Date.now();
     await prisma.$connect();
     logger.info("Database Connected")
-    // const connectTime = Date.now() - startTime;
-    // console.log(` Database connected in ${connectTime}ms`);
   } catch (error) {
     console.error("Database connection failed:", error);
   }
