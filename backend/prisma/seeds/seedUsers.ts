37608e1a5c749765da0d4d040dd191e56185cbb2
Retesh Mourya <opboy4885@gmail.com>
2026-04-21 23:37:50 +0530

    Update seedUsers.ts

--- backend/prisma/seeds/seedUsers.ts
+++ backend/prisma/seeds/seedUsers.ts
@@ -1,27 +1,27 @@
-import { prisma } from "../../src/lib/prisma";
-import bcrypt from "bcryptjs";
+// import { prisma } from "../../src/lib/prisma";
+// import bcrypt from "bcryptjs";
 
-export async function seedUsers() {
-  const adminPassword = await bcrypt.hash("", 10);
-  const johnPassword = await bcrypt.hash("", 10);
+// export async function seedUsers() {
+//   const adminPassword = await bcrypt.hash("", 10);
+//   const johnPassword = await bcrypt.hash("", 10);
 
-  await prisma.user.createMany({
-    data: [
-      {
-        name: "Admin",
-        email: "admin@bookverse.com",
-        password: adminPassword,
-        role: "admin",
-      },
-      {
-        name: "John Doe",
-        email: "john@example.com",
-        password: johnPassword,
-        role: "user",
-      },
-    ],
-    skipDuplicates: true,
-  });
+//   await prisma.user.createMany({
+//     data: [
+//       {
+//         name: "Admin",
+//         email: "admin@bookverse.com",
+//         password: adminPassword,
+//         role: "admin",
+//       },
+//       {
+//         name: "John Doe",
+//         email: "john@example.com",
+//         password: johnPassword,
+//         role: "user",
+//       },
+//     ],
+//     skipDuplicates: true,
+//   });
 
-  console.log("Users seeded successfully");
-}
+//   console.log("Users seeded successfully");
+// }
